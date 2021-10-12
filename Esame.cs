6e3761acd174670e60d308698218796f2b2088ac
leonardieuris <92081118+leonardieuris@users.noreply.github.com>
2021-10-12 18:01:42 +0200

    Initial commit

---
+++ Esame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scuola
+{
+    public class Esame
+    {
+        public int Voto { get; set; }
+        public Materie Materia { get; set; }
+        public Insegnanti Insegnante { get; set; }
+        public Studenti Studenti { get; set; }
+        public DateTime Data { get; set; }
+
+        public bool Lode { get; set; }
+
+
+    }
+}
---
+++ Persone.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Scuola
+{
+    public class Persone
+    { 
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string Id { get; set; }
+
+    }
+}
